70981c6b5806e2e4f216011393145ad33e6693fa
alexandramarum <alexandramarum@gmail.com>
2025-02-15 13:48:33 -0500

    initial commit

---
+++ pearlhacks/pearlhacks/pearlhacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  pearlhacksApp.swift
+//  pearlhacks
+//
+//  Created by Alexandra Marum on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct pearlhacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pearlhacks/pearlhacksTests/pearlhacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  pearlhacksTests.swift
+//  pearlhacksTests
+//
+//  Created by Alexandra Marum on 2/15/25.
+//
+
+import Testing
+@testable import pearlhacks
+
+struct pearlhacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
