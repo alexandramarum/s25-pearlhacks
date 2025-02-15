2a1debad98067319c6c24ebc541bffbf8334f100
alexandramarum <alexandramarum@gmail.com>
2025-02-15 16:20:40 -0500

    code organization

---
+++ pearlhacks/pearlhacks/Home/ListingView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  pearlhacks
+//
+//  Created by Alexandra Marum on 2/15/25.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingView()
+}
