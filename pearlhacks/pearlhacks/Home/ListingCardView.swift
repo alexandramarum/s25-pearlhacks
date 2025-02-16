1167caa3a1d20af3a2cacac48e3f1e95ae693890
alexandramarum <alexandramarum@gmail.com>
2025-02-15 21:04:08 -0500

    Prevent merge conflict

--- pearlhacks/pearlhacks/Home/ListingCardView.swift
+++ pearlhacks/pearlhacks/Home/ListingCardView.swift
@@ -8,25 +8,11 @@
 import SwiftUI
 
 struct ListingCardView: View {
-    var listing: Listing
-    
     var body: some View {
-        VStack(alignment: .leading) {
-            Image(listing.image)
-                .resizable()
-                .scaledToFill()
-                .frame(width: 375, height: 400)
-                .clipShape(RoundedRectangle(cornerRadius: 20.0))
-            Text("$\(listing.price.description)")
-                .bold()
-                .font(.largeTitle)
-            Text("$\(listing.mortgage.description)/mo")
-                .font(.title2)
-                .foregroundStyle(.secondary)
-        }
+        Text("Hello, world!")
     }
 }
 
 #Preview {
-    ListingCardView(listing: Listing.example)
+    ListingCardView()
 }
